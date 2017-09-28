23106b5e37647035f45ad92eb638d760eb26414d
Sneh Pandya <sneh.pandya1@gmail.com>
2017-09-28 12:27:44 +0530

    Implement: From operator

--- app/src/main/java/com/snehpandya/rxrecipes/MainActivity.java
+++ app/src/main/java/com/snehpandya/rxrecipes/MainActivity.java
@@ -17,7 +17,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Simple implementation of Observable and Subscriber
+        /*
+            Simple implementation of Observable and Subscriber
+
+            !!Key Idea 1: Observable and Subscriber can do anything.
+
+            !!Ket Idea 2: Observable and Subscriber are independent
+            of the transformational steps between them.
+
+            !!Key Idea 3: Hot vs Cold Observables ->
+            Emits Items all the time even when no one is listening
+            vs
+            Only emits items when it has a subscriber
+        */
+
         Observable.just("Hello World").subscribe(s -> Log.d(TAG, "onCreate: Just: " + s));
 
         Article article = new Article();
@@ -40,5 +53,15 @@ public class MainActivity extends AppCompatActivity {
                 .map(String::hashCode)
                 .map(i -> Integer.toString(i))
                 .subscribe(s -> Log.d(TAG, "onCreate: Map: " + s));
+
+        /*
+            **Observable.from() operator**
+
+            Takes a collection of objects/items and emits each of them, one at a time
+
+            !!Tip: Similar to Observable.just() operator
+        */
+
+        Observable.fromArray(new Integer[]{1, 2, 3, 4, 5}).subscribe(i -> Log.d(TAG, "onCreate: From: " + i));
     }
 }
